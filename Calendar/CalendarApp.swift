511a447777b77e7ff875600ca57c0e0b140138ce
Imacder <sunlili209@gmail.com>
2022-12-25 15:22:41 +0800

    Initial Commit

---
+++ Calendar/CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarApp.swift
+//  Calendar
+//
+//  Created by Albert Kong on 2022/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
